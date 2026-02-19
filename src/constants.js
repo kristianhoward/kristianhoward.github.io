45e76d34e6661ed73f9a659fcec99fc865272d31
kristianhoward <bbkkristian@gmail.com>
2026-02-19 08:20:52 -0800

    Update build asset paths and fix background path
    Updated the CSS to use a production-friendly background path (./img/background.webp).

--- src/constants.js
+++ src/constants.js
@@ -1,20 +1,44 @@
-export const unity_img_path = "./src/assets/img/unity.png";
-export const python_img_path = "./src/assets/img/python.png";
+import unityAlt from "./assets/img/unity_alt.png"
+import pythonAlt from "./assets/img/python_alt.png"
+import marimoAlt from "./assets/img/marimo_alt.png"
+import playwrightAlt from "./assets/img/playwright_alt.png"
+import seleniumAlt from "./assets/img/selenium_alt.png"
+import sqlAlt from "./assets/img/sql_alt.png"
+import csharpAlt from "./assets/img/csharp_alt.png"
+import piAlt from "./assets/img/pi_alt.png"
+import jsonAlt from "./assets/img/json_alt.png"
+import claudeAlt from "./assets/img/claude_alt.png"
+import javascriptAlt from "./assets/img/javascript_alt.png"
+import tornadoAlt from "./assets/img/tornado_alt.png"
+import qaAlt from "./assets/img/qa_alt.png"
+import pytestAlt from "./assets/img/pytest_alt.png"
+import linkedinPath from "./assets/img/linkedin_alt.png"
+import githubPath from "./assets/img/github_alt.png"
+import rcodPath from "./assets/img/rcod_1.png"
+import dndPath from "./assets/img/dnddisplay.png"
+import mapPath from "./assets/img/mapvalidation.png"
+import automationPath from "./assets/img/automation.png"
+import qaPath from "./assets/img/qa.png";
+import unityPath from "./assets/img/unity.png"
+import pythonPath from "./assets/img/python.png"
 
-export const unity_alt_path = "./src/assets/img/unity_alt.png";
-export const python_alt_path = "./src/assets/img/python_alt.png";
-export const marimo_alt_path = "./src/assets/img/marimo_alt.png";
-export const playwright_alt_path = "./src/assets/img/playwright_alt.png";
-export const selenium_alt_path = "./src/assets/img/selenium_alt.png";
-export const sql_alt_path = "./src/assets/img/sql_alt.png";
-export const csharp_alt_path = "./src/assets/img/csharp_alt.png";
-export const rasp_pi_alt_path = "./src/assets/img/pi_alt.png";
-export const json_alt_path = "./src/assets/img/json_alt.png";
-export const claude_alt_path = "./src/assets/img/claude_alt.png";
-export const javascript_alt_path = "./src/assets/img/javascript_alt.png";
-export const tornado_alt_path = "./src/assets/img/tornado_alt.png";
-export const qa_alt_path = "./src/assets/img/qa_alt.png";
-export const pytest_alt_path = "./src/assets/img/pytest_alt.png";
+export const unity_img_path = unityPath;
+export const python_img_path = pythonPath;
+
+export const unity_alt_path = unityAlt;
+export const python_alt_path = pythonAlt;
+export const marimo_alt_path = marimoAlt;
+export const playwright_alt_path = playwrightAlt;
+export const selenium_alt_path = seleniumAlt;
+export const sql_alt_path = sqlAlt;
+export const csharp_alt_path = csharpAlt;
+export const rasp_pi_alt_path = piAlt;
+export const json_alt_path = jsonAlt;
+export const claude_alt_path = claudeAlt;
+export const javascript_alt_path = javascriptAlt;
+export const tornado_alt_path = tornadoAlt;
+export const qa_alt_path = qaAlt;
+export const pytest_alt_path = pytestAlt;
 
 export const technologies = {
     "Unity": unity_alt_path,
@@ -33,19 +57,17 @@ export const technologies = {
     "Pytest": pytest_alt_path,
 }
 
-export const linkedin_path = "./src/assets/img/linkedin_alt.png";
-export const github_path = "./src/assets/img/github_alt.png";
+export const linkedin_path = linkedinPath;
+export const github_path = githubPath;
 
 export const linkedin_link = "https://www.linkedin.com/in/kristian-howard-jr/"
 export const github_link = "https://github.com/kristianhoward"
 
-export const bg_video_path = "./src/assets/vid/web_bg.mp4";
-
 export const home_display_img_paths = [
-    "./src/assets/img/rcod_1.png",
-    "./src/assets/img/dnddisplay.png",
-    "./src/assets/img/mapvalidation.png",
-    "./src/assets/img/automation.png",
+    rcodPath,
+    dndPath,
+    mapPath,
+    automationPath
 ]
 
 export const imageNames = [
@@ -54,4 +76,4 @@ export const imageNames = [
     "GIS Map Validation",
     "Traffic Signal Controller Automation"
 ]
-export const qa_img_path = "./src/assets/img/qa.png";
+export const qa_img_path = qaPath;
